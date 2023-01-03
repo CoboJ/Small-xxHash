b1726a8c35831de1adbca35aa7bef693a5fac178
Jorge Cobo <cobo-angel@hotmail.com>
2023-01-03 11:49:45 -0400

    Generated first uv image

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
+use image::Rgb;
+
+extern crate image;
+/*
 struct HashVisualization {
     resolution : i32,
     hashes : Vec<u32>,
-}
+}*/
 
 fn main() {
-    let hashVisualization = HashVisualization {
-        resolution : 16,
-        hashes : vec![0; 16*16]
-    };
+    let mut imgbuf: image::ImageBuffer<Rgb<u8>, Vec<_>> = image::ImageBuffer::new(16 as u32, 16 as u32);
 
-    println!("Hello, world!");
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = ((x as f32/16.0) * 255.0) as u8;
+        let g = ((y as f32/16.0) * 255.0) as u8;
+        *pixel = image::Rgb([r, g, 0]);
+    }
+    //let v : i32 =  
+    //let u : i32 = 
+    imgbuf.save("test.png").unwrap();
+
+    println!("Done! Image ready!");
 }
