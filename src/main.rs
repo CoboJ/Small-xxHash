9c339c31fe8c1e5bafa0cb3192855236a468f2ee
Jorge Cobo <cobo-angel@hotmail.com>
2023-01-05 11:03:56 -0400

    frac function to get fractional part of a float added

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
-use image::Rgb;
+use image::*;
 
-extern crate image;
 /*
 struct HashVisualization {
     resolution : i32,
     hashes : Vec<u32>,
 }*/
 
+fn frac(x: f32) -> f32 {
+    x - x.floor()
+}
+
 fn main() {
-    let mut imgbuf: image::ImageBuffer<Rgb<u8>, Vec<_>> = image::ImageBuffer::new(16 as u32, 16 as u32);
+    let mut imgbuf: image::ImageBuffer<Luma<u8>, Vec<_>> = image::ImageBuffer::new(32 as u32, 32 as u32);
 
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = ((x as f32/16.0) * 255.0) as u8;
-        let g = ((y as f32/16.0) * 255.0) as u8;
-        *pixel = image::Rgb([r, g, 0]);
+        let hash = ((x + 1) * (y + 1)) as f32;
+        println!("Hash value: '{hash}' based on: '({x} + 1) * ({y} + 1)'");
+        let g = (frac(hash * 0.381) * 256.0) as u8;
+        *pixel = image::Luma([g]);
     }
     //let v : i32 =  
     //let u : i32 = 
