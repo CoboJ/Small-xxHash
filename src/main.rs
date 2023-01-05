fb93484f194d9904c9803440e9b017a7fe8d97f5
Jorge Cobo <cobo-angel@hotmail.com>
2023-01-05 11:29:56 -0400

    Generating image with Weyl sequence

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ fn main() {
     let mut imgbuf: image::ImageBuffer<Luma<u8>, Vec<_>> = image::ImageBuffer::new(32 as u32, 32 as u32);
 
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let hash = ((x + 1) * (y + 1)) as f32;
-        println!("Hash value: '{hash}' based on: '({x} + 1) * ({y} + 1)'");
+        let hash = (x + y) as f32;
+        println!("Hash value: '{hash}' based on: '{x} + {y}'");
         let g = (frac(hash * 0.381) * 256.0) as u8;
         *pixel = image::Luma([g]);
     }
