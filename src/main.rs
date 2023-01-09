acec733102cfc656c613be34aacb708973d398ae
Jorge Cobo <cobo-angel@hotmail.com>
2023-01-09 09:58:58 -0400

    Iterating through pixels index instead of the pixels coordinates 'uv'

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,9 @@ fn frac(x: f32) -> f32 {
 fn main() {
     let mut imgbuf: image::ImageBuffer<Luma<u8>, Vec<_>> = image::ImageBuffer::new(41 as u32, 41 as u32);
     
-    let mut i = 0;
-    for (_x, _y, pixel) in imgbuf.enumerate_pixels_mut() {
+    for (i, pixel) in imgbuf.pixels_mut().enumerate() {
         let g = (frac(i as f32 * 0.381) * 256.0) as u8;
-        println!("Hash value: '{g}' at index: '{i}'");
         *pixel = image::Luma([g]);
-        i += 1;
     }
 
     imgbuf.save("test.png").unwrap();
