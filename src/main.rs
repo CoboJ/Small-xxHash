6d25d3830133c2a6ae1c5296eb72a300ada1f9b5
Jorge Cobo <cobo-angel@hotmail.com>
2023-01-09 10:57:35 -0400

    Generating weyl sequence in image iterating by coordinate

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,20 @@ fn frac(x: f32) -> f32 {
 }
 
 fn main() {
-    let mut imgbuf: image::ImageBuffer<Luma<u8>, Vec<_>> = image::ImageBuffer::new(41 as u32, 41 as u32);
+    let mut imgbuf: image::ImageBuffer<Luma<u8>, Vec<_>> = image::ImageBuffer::new(32 as u32, 32 as u32);
     
+    /*let resolution = 32 * 32;
+    let invResolution = 1.0 / resolution as f32;
     for (i, pixel) in imgbuf.pixels_mut().enumerate() {
-        let g = (frac(i as f32 * 0.381) * 256.0) as u8;
+        let v = (invResolution * i as f32 + 0.00001).floor();
+        let u = (i as f32 - resolution as f32 * v);
+        let g = (frac(u * v * 0.381) * 256.0) as u8;
+        println!("U: '{u}' V: '{v}' Grayscale: '{g}'");
+        *pixel = image::Luma([g]);
+    }*/
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let g = (frac(x as f32 * (31 - y) as f32 * 0.381) * 256.0) as u8;
         *pixel = image::Luma([g]);
     }
 
