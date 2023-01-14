d31b5e5b169daacbc84a51efde4ac509d844417d
Jorge Cobo <cobo-angel@hotmail.com>
2023-01-14 09:55:26 -0400

    Generating colored hash images

--- src/hash_visualization.rs
+++ src/hash_visualization.rs
@@ -4,16 +4,33 @@ use crate::hash::SmallXXHash;
 pub struct HashVisualization;
 
 impl HashVisualization {
-    pub fn generate_image(width : u32, height: u32, path: String)
+    pub fn noise_img(width : u32, height: u32, path: String)
     {
         let mut imgbuf: image::ImageBuffer<Luma<u8>, Vec<_>> = image::ImageBuffer::new(width, height);
 
         for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
             let u = x as i32 - (width / 2) as i32;
             let v = (height / 2) as i32 - y as i32;
-            let g = SmallXXHash::new(0).eat(u).eat(v).get() as u8;
-            println!("U: '{u}' V: '{v}' Grayscale: '{g}'");
+            let hash = SmallXXHash::new(0).eat(u).eat(v).get();
+            let g = hash as u8;
             *pixel = image::Luma([g]);
+        }
+
+        imgbuf.save(path).unwrap();
+    }
+
+    pub fn color_img(width : u32, height: u32, path: String)
+    {
+        let mut imgbuf: image::ImageBuffer<Rgb<u8>, Vec<_>> = image::ImageBuffer::new(width, height);
+
+        for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+            let u = x as i32 - (width / 2) as i32;
+            let v = (height / 2) as i32 - y as i32;
+            let hash = SmallXXHash::new(0).eat(u).eat(v).get();
+            let r = hash as u8;
+            let g = hash.wrapping_shr(8) as u8;
+            let b = hash.wrapping_shr(16) as u8;
+            *pixel = image::Rgb([r, g, b]);
         }
 
         imgbuf.save(path).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod hash_visualization;
 use hash_visualization::HashVisualization;
 
 fn main() {
-    HashVisualization::generate_image(32, 32, String::from("test.png"));
+    HashVisualization::
+        color_img(32, 32, String::from("test.png"));
     println!("Done! Image ready!");
 }
